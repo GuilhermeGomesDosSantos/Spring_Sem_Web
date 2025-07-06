52a109d27a15fd96c496b62121dc6f2ed338d8b8
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-07-06 16:38:56 -0300

    Projeto Spring sem web

---
+++ src/main/java/br/com/alura/screnmatch/ScrenmatchApplication.java
@@ -0,0 +1,18 @@
+package br.com.alura.screnmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro Projeto Spring, sem Web!");
+	}
+}
