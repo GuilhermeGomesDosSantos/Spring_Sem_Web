049ed6646c1dd7385a3fdcb488c34f9c40ec8f75
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-08-29 21:16:07 -0300

    Buscar por trecho de titulo

--- src/main/java/br/com/alura/screnmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screnmatch/Principal/Principal.java
@@ -35,6 +35,7 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar Séries buscadas
+                    4 - Buscar Série por titulo
                     
                     0 - Sair                                 
                     """;
@@ -52,6 +53,9 @@ public class Principal {
                     break;
                 case 3:
                     listarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -86,10 +90,11 @@ public class Principal {
         System.out.println("Escolha uma Série pelo nome: ");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serie = series
-                .stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+//        Optional<Serie> serie = series
+//                .stream()
+//                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
+//                .findFirst();
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
         if (serie.isPresent()) {
             var serieEncontrada = serie.get();
@@ -119,5 +124,18 @@ public class Principal {
             series.stream()
                             .sorted(Comparator.comparing(Serie::getGenero))
                                     .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolhe uma série pelo Titulo");
+        var nomeSerie = leitura.nextLine();
+
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBuscada.isPresent()){
+            System.out.println("Série encontrada: " + serieBuscada.get());
+        } else {
+            System.out.println("Série não encontrada");
+        }
     }
 }
--- src/main/java/br/com/alura/screnmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screnmatch/repository/SerieRepository.java
@@ -3,5 +3,8 @@ package br.com.alura.screnmatch.repository;
 import br.com.alura.screnmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
 }
