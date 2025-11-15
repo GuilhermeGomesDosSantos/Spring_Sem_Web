35898772449e58cae71b6f7b9ec5738acbe68c3c
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-11-15 18:57:12 -0300

    Exibindo uma mensagem no navegador

--- src/main/java/br/com/alura/screnmatch/ScrenmatchApplication.java
+++ src/main/java/br/com/alura/screnmatch/ScrenmatchApplication.java
@@ -16,18 +16,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-public class ScrenmatchApplication implements CommandLineRunner {
-
-	@Autowired
-	private SerieRepository repositorio;
+public class ScrenmatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 	}
 }
