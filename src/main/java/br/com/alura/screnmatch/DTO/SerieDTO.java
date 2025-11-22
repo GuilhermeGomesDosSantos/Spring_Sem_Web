ac74ffbb3d639e15b92d29cfc0d0fd05debbd029
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-11-22 20:30:00 -0300

    Adicionando politica de CORS

--- src/main/java/br/com/alura/screnmatch/DTO/SerieDTO.java
+++ src/main/java/br/com/alura/screnmatch/DTO/SerieDTO.java
@@ -1,2 +1,17 @@
-package br.com.alura.screnmatch.DTO;public record SerieDTO() {
+package br.com.alura.screnmatch.DTO;
+
+import br.com.alura.screnmatch.model.Categoria;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+public record SerieDTO(
+        Long id,
+        String titulo,
+        Integer totalTemporadas,
+        Double avaliacao,
+        Categoria genero,
+        String atores,
+        String poster,
+        String sinopse
+) {
 }
