f7d98d278626ab230762685c01281c55487c0186
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-11-22 20:57:03 -0300

    Criando um serviço para Série

--- src/main/java/br/com/alura/screnmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screnmatch/controller/SerieController.java
@@ -3,6 +3,7 @@ package br.com.alura.screnmatch.controller;
 import br.com.alura.screnmatch.DTO.SerieDTO;
 import br.com.alura.screnmatch.model.Serie;
 import br.com.alura.screnmatch.repository.SerieRepository;
+import br.com.alura.screnmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,10 @@ import java.util.stream.Collectors;
 public class SerieController {
 
     @Autowired
-    private SerieRepository repository;
+    private SerieService service;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries() {
-        return repository.findAll().
-                stream().
-                map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return service.obterSeries();
     }
 }
---
+++ src/main/java/br/com/alura/screnmatch/service/SerieService.java
@@ -0,0 +1,23 @@
+package br.com.alura.screnmatch.service;
+
+import br.com.alura.screnmatch.DTO.SerieDTO;
+import br.com.alura.screnmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repository;
+
+    public List<SerieDTO> obterSeries(){
+        return repository.findAll().
+                stream().
+                map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
